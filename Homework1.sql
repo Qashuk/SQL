b4301bc6bbff1ad2e2396f323a62713edbdf36f2
Qashuk <125905858+Qashuk@users.noreply.github.com>
2023-03-25 13:46:58 +0200

    Create Homework1.sql

---
+++ Homework1.sql
@@ -0,0 +1,9 @@
+Таблица employees
+
+1. Создать таблицу employees
+- id. serial,  primary key,
+- employee_name. Varchar(50), not null
+  create table employees (
+  id serial primary key,
+  employee_name varchar(50) not null 
+);
